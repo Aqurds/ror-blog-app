90ea1de2c498d2c2c6c9fd6c28747778c0cbc515
aqurds <omarf1320@gmail.com>
2023-10-03 17:30:23 +0600

    Update requested changes by the reviewer

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,9 @@
 Rails.application.routes.draw do
-  # resources :likes
-  # resources :users
-  # resources :comments
-  # resources :posts
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  get '/users', to: 'users#index'
-  get '/users/:id', to: 'users#show', as: 'show_a_user'
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show]
+  end
   
-  get '/users/:user_id/posts', to: 'posts#index', as: 'show_a_user_posts_comments'
-
-  get '/users/:user_id/posts/:id', to: 'posts#show', as: 'show_a_user_a_post_comments'
-
-  root 'users#index'
-
+  # Set the root route to the Users#index action
+  root 'users#index'  
+  
 end
